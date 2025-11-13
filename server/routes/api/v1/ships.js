520becf12c1779548564e495c3daa27762de1a76
Alexander Hansen <300071283@fvtc.edu>
2025-11-13 03:03:04 -0600

    added full api for users and ships (for now) users is not working fully and I am working on refactoring some code

--- server/routes/api/v1/ships.js
+++ server/routes/api/v1/ships.js
@@ -1,41 +1,58 @@
 const router = require('express').Router()
 const sqlDb = require('../../../db_connection')
 
+//*add the name of table this part of the api will be working on
+const tableName = 'spaceships'
+
 router.get('/', async (req, res) => {
-    res.send('you made it to users')
+    res.send('you made it to the end point')
 })
 
 router.get('/:id', async (req, res) =>{
     const { id } = req.params
 
-    const query = `Select * From spaceships as s
-                    Where s.id = ${id}`
+    const query = `Select * From ${tableName} as s
+                    Where s.id = ?`
 
-    const ship = await sqlDb.promise().query(query)
+    const item = await sqlDb.promise().query(query, [id])
 
-    res.send(ship[0].length === 0 ? { error : `oops looks like we couldn't find the spaceship with id:${id}` } : ship[0][0])
+    res.send(item[0].length === 0 ? { error : `oops looks like we couldn't find the ${tableName} with id:${id}` } : item[0][0])
 })
 
 router.put('/create', async (req, res) => {
     const { Name, Universe } = req.body
     
     const query = `Insert Into 
-                    SpaceShip (Name, Universe)
+                    ${tableName} (Name, Universe)
                     Values ( ?, ?)`
-    //*this is parameterized data in mysql2
-    const addedShip = await sqlDb.promise().query(query, [Name, Universe])
+    //*this is parameterized data in mysql2 aka the ? and then query(query, [var, var]) where ? is var
+    const addedItem = await sqlDb.promise().query(query, [Name, Universe])
     
     //TODO: don't yet know how to get the data to come down into the API from the front end... need to figure this out
-    res.send(addedShip)
+    res.send(addedItem)
 })
 
 router.post('/update', async (req, res) => {
+    const { id, Name, Universe } = req.body
 
+    const query = `Update ${tableName}
+                    Set Name = ?, Universe = ?
+                    Where id = ?`
+    
+    const updatedItem = await sqlDb.promise().query(query, [Name, Universe, id])
+
+    res.send(updatedItem)
 })
 
-router.delete('/delete', async (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params
 
+    const query = `Delete From ${tableName}
+                    Where id = ?`
+    
+    const deletedItem = await sqlDb.promise().query(query, [id])
+
+    res.send(deletedItem)
 })
-
 
 module.exports = router
--- server/routes/api/v1/users.js
+++ server/routes/api/v1/users.js
@@ -1,32 +1,81 @@
 const router = require('express').Router()
 const sqlDb = require('../../../db_connection')
 
+//*add the name of table this part of the api will be working on
+const tableName = 'users'
+
+const obfuscateSqlParams = (reqBody, doAssignment = false) => { 
+        const reqKeys = Object.keys(reqBody)
+        console.log(reqBody)
+        return reqKeys.map(e => doAssignment ? `${e} = ?` : '?'
+)}   
+
 router.get('/', async (req, res) => {
-    res.send('you made it to users')
+    res.send('you made it to the end point')
 })
 
 router.get('/:id', async (req, res) =>{
     const { id } = req.params
 
-    const query = `Select * From users as u
-                    Where u.id = ${id}`
+    const query = `Select * From ${tableName} as s
+                    Where s.id = ?`
 
-    const user = await sqlDb.promise().query(query)
+    const item = await sqlDb.promise().query(query, [id])
 
-    res.send(user[0].length === 0 ? { error : `oops looks like we couldn't find the user with id:${id}` } : user[0][0])
+    res.send(item[0].length === 0 ? { error : `oops looks like we couldn't find the ${tableName} with id:${id}` } : item[0][0])
 })
 
 router.put('/create', async (req, res) => {
+    const reqBody = { 
+        First_Name, 
+        Last_Name,
+        User_Name,
+        Email,
+        Password
+    } = req.body
 
+    const query = `Insert Into 
+                    ${tableName} (${Object.keys(reqBody)})
+                    Values (${obfuscateSqlParams(reqBody)})`
+
+    const addedItem = await sqlDb.promise().query(query, Object.values(reqBody))
+    
+    //TODO: don't yet know how to get the data to come down into the API from the front end... need to figure this out
+    res.send(addedItem)
 })
 
 router.post('/update', async (req, res) => {
+    const reqBody = {
+        First_Name,
+        Last_Name,
+        User_Name,
+        Email,
+        Password
+    } = req.body
 
+    //!hacky ahhh fix for the fact that for some reason the reqBody obj comes back with id even tho I dont tell it to...
+    delete reqBody.id
+
+    const query = `Update ${tableName}
+                    Set (${obfuscateSqlParams(reqBody, true)})
+                    Where id = ?`
+    //Object.values(reqBody).push(req.body.id)
+    const { id } = req.body
+    console.log(query)
+    const updatedItem = await sqlDb.promise().query(query, [First_Name, Last_Name, User_Name, Email, Password, id])
+
+    res.send(updatedItem)
 })
 
-router.delete('/delete', async (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params
 
+    const query = `Delete From ${tableName}
+                    Where id = ?`
+    
+    const deletedItem = await sqlDb.promise().query(query, [id])
+
+    res.send(deletedItem)
 })
-
 
 module.exports = router
